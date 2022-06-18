b70bda614a5c2e8e2c7f46d322c6e345dea393b4
harsh9539 <goyalh862@gmail.com>
2022-06-18 16:24:36 +0530

    adding route for products ID

--- controller/product-controller.js
+++ controller/product-controller.js
@@ -11,3 +11,13 @@ export const getProducts = async (request,response)=>{
         response.status(500).json({message:err.message});
     }
 }
+export const getProductById = async (request,response)=>{
+    try{
+        const id = request.params.id;
+        const product = await Product.findOne({'id':id})
+        response.status(200).json(product);
+
+    }catch(err){
+        response.status(500).json({message:err.message});
+    }
+}
