ac36c7cad2bc14dbfe7b1c9bb3a2c66de3771115
harsh9539 <goyalh862@gmail.com>
2022-07-03 08:34:47 +0530

    adding paytm payment gateway

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 import { Connection } from './DataBase/db.js';
 import DefaultData from './default.js';
 import router from './routes/route.js';
-
+import {v4 as uuid} from 'uuid';
 const app = express();
 
 dotenv.config();
@@ -26,3 +26,21 @@ const PASSWORD = process.env.DB_PASSWORD;
 Connection(USERNAME,PASSWORD);
 app.listen(PORT,()=>console.log(`server is running on port ${PORT} `))
 DefaultData();
+
+
+
+export let paytmMerchantKey = process.env.PAYTM_MERCHANT_KEY;
+export let paytmParams ={};
+paytmParams['MID'] = process.env.PAYTM_MID
+paytmParams['WEBSITE'] = process.env.PAYTM_WEBSITE
+paytmParams['CHANNEL_ID'] = process.env.PAYTM_CHANNEL_ID
+paytmParams['INDUSTRY_TYPE_ID'] = process.env.PAYTM_INDUSTRY_TYPE
+paytmParams['ORDER_ID'] = uuid();
+paytmParams['CUST_ID'] = process.env.PAYTM_CUST_ID;
+paytmParams['TXN_AMOUNT'] = '100';
+paytmParams['CALLBACK_URL'] = 'http://localhost:8000/callback';
+paytmParams['EMAIL'] = 'harsh9539@gmail.com';
+paytmParams['MOBILE_NO'] = '1234567890';
+
+
+
